6e220f48677ecd8aad28488cc0e1d474f9e62e0e
Delanie Perez <delanieperez11@gmail.com>
2018-05-04 22:25:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray(array, element) {
   return ["foo", ...array]
 }
+
+function addElementToBeginningOfArray(array, element) {
+  return ["foo", ...array]
+}
