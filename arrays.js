a14c0f1c1755029711010267b65c86ae76531168
Delanie Perez <delanieperez11@gmail.com>
2018-05-04 22:27:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,5 +5,6 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return ["foo", array]
+ array.unshift (element)
+ return array
 }
